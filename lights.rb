430633810902ccae1bf76d7c95df1b37011ed259
urthbound <jeffowler@gmail.com>
2015-03-22 19:10:17 -0400

    first draft with just button on

--- lights.rb
+++ lights.rb
@@ -8,7 +8,11 @@ ser = SerialPort.new("/dev/tty.usbserial-m1000065", 9600)
 loop do
     der = ser.readline(1).ord.to_s(16).to_i
     if der == 21
-        ser.write("\x13")
+        x = ser.readline(1).ord.to_s(16)
+        y = ser.readline(1).ord.to_s(16)
+        ser.write("\x11")
+        ser.write(x)
+        ser.write(y)
     end
 
     if der == 20
