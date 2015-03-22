d20db7640bbcf8798a8b47fc1a2b41ba7b773834
urthbound <jeffowler@gmail.com>
2015-03-22 19:20:34 -0400

    individual on off

--- lights.rb
+++ lights.rb
@@ -8,14 +8,18 @@ ser = SerialPort.new("/dev/tty.usbserial-m1000065", 9600)
 loop do
     der = ser.readline(1).ord.to_s(16).to_i
     if der == 21
-        x = ser.readline(1).ord.to_s(16)
-        y = ser.readline(1).ord.to_s(16)
+        x = ser.readline(1)
+        y = ser.readline(1)
         ser.write("\x11")
         ser.write(x)
         ser.write(y)
     end
 
     if der == 20
-        ser.write("\x12")
+        x = ser.readline(1)
+        y = ser.readline(1)
+        ser.write("\x10")
+        ser.write(x)
+        ser.write(y)
     end
 end
